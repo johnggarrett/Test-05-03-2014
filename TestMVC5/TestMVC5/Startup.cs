90fe2fa095c112faeb2d0db49022668caa53d5ff
johnggarrett <john_g_garrett@hotmail.co.uk>
2014-03-05 19:48:26 +0000

    Inital Version

---
+++ TestMVC5/TestMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMVC5.Startup))]
+namespace TestMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
